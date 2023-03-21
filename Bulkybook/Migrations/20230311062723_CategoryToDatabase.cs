03ac18f258f1003961f46979688656ba7ee8fa81
Prajesh Waghela <prajeshwaghela13@gmail.com>
2023-03-21 15:21:26 +0530

    BulkyBookMVC

---
+++ Bulkybook/Migrations/20230311062723_CategoryToDatabase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bulkybook.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Bulkybook/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bulkybook.Models
+{
+    public class Category 
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        
+        [Range(1,100,ErrorMessage = "Display error must be between 1 and 100 only!!")]
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDatetime { get; set; } = DateTime.Now;   
+    }
+}
+
